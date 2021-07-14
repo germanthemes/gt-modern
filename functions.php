b33cf98ef3726102528862850376d257df3c3288
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-07-14 14:57:53 +0200

    removed license key settings

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  * GT Modern only works in WordPress 5.3 or later.
  */
 if ( version_compare( $GLOBALS['wp_version'], '5.3', '<' ) ) {
-	require get_template_directory() . '/inc/admin/back-compat.php';
+	require get_template_directory() . '/inc/back-compat.php';
 	return;
 }
 
@@ -321,42 +321,8 @@ add_action( 'widgets_init', 'gt_modern_widgets_init' );
 
 
 /**
- * Set up automatic theme updates.
- *
- * @return void
- */
-function gt_modern_theme_updater() {
-	if ( '' !== gt_modern_get_option( 'license_key' ) ) :
-
-		// Setup the updater.
-		$theme_updater = new GT_Modern_Theme_Updater(
-			array(
-				'remote_api_url' => GT_MODERN_STORE_API_URL,
-				'version'        => '1.2.2',
-				'license'        => trim( gt_modern_get_option( 'license_key' ) ),
-				'item_id'        => GT_MODERN_PRODUCT_ID,
-				'item_name'      => 'GT Modern',
-				'theme_slug'     => 'gt-modern',
-				'author'         => 'GermanThemes',
-			),
-			array(
-				'update-notice'    => __( "Updating this theme will lose any customizations you have made. 'Cancel' to stop, 'OK' to update.", 'gt-modern' ),
-				'update-available' => __( '<strong>%1$s %2$s</strong> is available. <a href="%3$s" class="thickbox" title="%4$s">Check out what\'s new</a> or <a href="%5$s"%6$s>update now</a>.', 'gt-modern' ),
-			)
-		);
-
-	endif;
-}
-add_action( 'admin_init', 'gt_modern_theme_updater', 0 );
-
-
-/**
  * Include Files
  */
-
-// Include Admin Classes.
-require get_template_directory() . '/inc/admin/license-key.php';
-require get_template_directory() . '/inc/admin/theme-updater.php';
 
 // Include Customizer Options.
 require get_template_directory() . '/inc/customizer/customizer.php';
--- inc/customizer/default-options.php
+++ inc/customizer/default-options.php
@@ -85,8 +85,6 @@ function gt_modern_default_options() {
 		'navi_font'          => 'Barlow',
 		'navi_is_bold'       => false,
 		'navi_is_uppercase'  => false,
-		'license_key'        => '',
-		'license_status'     => 'inactive',
 	);
 
 	return apply_filters( 'gt_modern_default_options', $default_options );
